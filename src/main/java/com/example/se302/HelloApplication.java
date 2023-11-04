5e3d5da6aa36e4ae2a5473f2d258c02a39b157a4
Şamil Nugay <samil_nugay@hotmail.com>
2023-11-05 01:20:25 +0300

    testing for my project

--- src/main/java/com/example/se302/HelloApplication.java
+++ src/main/java/com/example/se302/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Helloo!");
+        stage.setTitle("Helloooo!");
         stage.setScene(scene);
         stage.show();
     }
