b486656fe0e00a4f045547ad861ac49a5f505781
ahmetalpsamur <ahmetalpsamur@gmail.com>
2023-12-02 13:07:12 +0300

    add new opening page and adding save button into syylabux.fxml

--- src/main/java/com/example/se302/Controllers.java
+++ src/main/java/com/example/se302/Controllers.java
@@ -3,14 +3,18 @@ package com.example.se302;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class Controllers {
+public class Controllers implements Initializable  {
     @FXML
     private Label welcomeText;
     @FXML
@@ -21,6 +25,10 @@ public class Controllers {
     private Parent root;
 
     @FXML
+    private ChoiceBox<String>myChoiceBox;
+    private String[]course={"SE 302","SE 115","CE 323"};
+
+    @FXML
     protected void openSyllabus(ActionEvent event) throws IOException {
         Stage stage = new Stage();
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("syllabus.fxml"));
@@ -28,6 +36,11 @@ public class Controllers {
         stage.setTitle("Helloooo!");
         stage.setScene(scene);
         stage.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        myChoiceBox.getItems().addAll(course);
 
     }
 }
