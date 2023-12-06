7a022cea63bb7e6b660e1281e0de78ab2ac71fd2
Ozge Ozer <104353749+ozgeozerr@users.noreply.github.com>
2023-12-06 14:36:10 +0300

    Improved GUI design, added Turkish Syllabus, fixed some bugs about javafx and in general

--- src/main/java/com/example/se302/App.java
+++ src/main/java/com/example/se302/App.java
@@ -13,7 +13,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 700, 700);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 460);
         stage.setTitle("Syllabus App");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/se302/Controllers.java
+++ src/main/java/com/example/se302/Controllers.java
@@ -6,7 +6,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
@@ -14,31 +13,28 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controllers implements Initializable  {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controllers implements Initializable {
+
     @FXML
     private Label welcomeText;
-    @FXML
-    private Stage stage;
-    @FXML
-    private Scene scene;
-    @FXML
-    private Parent root;
-
-
-
-    @FXML
-    protected void openSyllabus(ActionEvent event) throws IOException {
-        Stage stage = new Stage();
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("syllabus.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 760, 750);
-        stage.setTitle("Syllabus");
-        stage.setScene(scene);
-        stage.show();
-    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Initialization code if needed
+    }
 
 
     }
-}
+
+
+
+
