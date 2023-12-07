3cc34419a03c06fe69fc57e93db06ac017173354
Şamil Nugay <samil_nugay@hotmail.com>
2023-12-08 01:08:27 +0300

    id added to textfields

--- src/main/java/com/example/se302/Controllers.java
+++ src/main/java/com/example/se302/Controllers.java
@@ -4,18 +4,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
-
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -25,16 +16,47 @@ import java.util.ResourceBundle;
 public class Controllers implements Initializable {
 
     @FXML
-    private Label welcomeText;
+    private TextField courseNameText;
+    @FXML
+    private TextField codeTextField;
+    @FXML
+    private TextField fallTextField;
+    @FXML
+    private TextField springTextField;
+    @FXML
+    private TextField theoryTextField;
+    @FXML
+    private TextField localCreditTextField;
+    @FXML
+    private TextField ectsTextField;
+    @FXML
+    private TextField prerequisitesTextField;
+    @FXML
+    private TextField courseLanguageTextField;
+    @FXML
+    private TextField courseTypeTextField;
+    @FXML
+    private TextField courseLevelTextField;
+    @FXML
+    private TextField teachingMethodsTextField;
+    @FXML
+    private TextField courseCoordinatorTextField;
+    @FXML
+    private TextField courseCoordinatorTextField2; // maybe change the name
+    @FXML
+    private TextField assistantTextField;
+    @FXML
+    private TextField courseObjectivesTextField;
+    @FXML
+    private TextField learningOutcomesTextField;
+    @FXML
+    private TextField courseDescriptionTextField;
+    @FXML
+    private TextField courseNameTextField2; // maybe change the name
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         // Initialization code if needed
     }
 
-
-    }
-
-
-
-
+}
