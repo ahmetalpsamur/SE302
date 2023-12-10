f23e824bf85566b869b69753a4fc24728da652f2
kadiratikk <atik.kadir@hotmail.com>
2023-12-10 17:25:23 +0300

    fix jsonread

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module com.example.se302 {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires com.fasterxml.jackson.databind;
+
+
     opens com.example.se302 to javafx.fxml;
     exports com.example.se302;
 }
